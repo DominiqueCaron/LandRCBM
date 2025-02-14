766a13fdcb3d3d402aacec809318f7c08c1e225d
DominiqueCaron <dominique.caron@mail.mcgill.ca>
2025-02-14 07:43:00 -0800

    working small area in RIA

--- global.R
+++ global.R
@@ -7,25 +7,25 @@ if (!require("SpaDES.project")){
 out <- SpaDES.project::setupProject(
   paths = list(projectPath = getwd(),
                inputPath = "inputs",
-               outputPath = "output",
+               outputPath = "outputs",
                cachePath = "cache"),
   options = options(spades.moduleCodeChecks = FALSE,
                     spades.recoveryMode = FALSE),
-  times = list(start = 2011, end = 2015),
-  modules = c("PredictiveEcology/Biomass_speciesFactorial@development",
-              "PredictiveEcology/Biomass_borealDataPrep@development",
-              "PredictiveEcology/Biomass_speciesParameters@development",
-              "DominiqueCaron/Biomass_yieldTables@main",
-              "DominiqueCaron/LandRCBM_split3pools@module-rewriting",
-              "PredictiveEcology/Biomass_core@main"
+  times = list(start = 2011, end = 2061),
+  modules = c(
+    "PredictiveEcology/Biomass_speciesFactorial@development",
+    "PredictiveEcology/Biomass_borealDataPrep@development",
+    "PredictiveEcology/Biomass_speciesParameters@development",
+    "DominiqueCaron/Biomass_yieldTables@main",
+    "PredictiveEcology/Biomass_core@main",
+    "DominiqueCaron/LandRCBM_split3pools@module-rewriting"
   ),
   params = list(
     .globals = list(
       dataYear = 2011, #will get kNN 2011 data, and NTEMS 2011 landcover
       .plots = c("png"),
-      sppEquivCol = LandR::equivalentNameColumn(
-        c("Abie_las", "Betu_pap", "Pice_gla", "Pice_mar", "Pinu_con", "Popu_tre",
-          "Pice_eng"), LandR::sppEquivalencies_CA), ".studyAreaName" = "RIA"
+      sppEquivCol = 'LandR',
+      .studyAreaName = "RIA"
     ),
     Biomass_borealDataPrep = list(
       .studyAreaName = "RIA",
@@ -52,26 +52,43 @@ out <- SpaDES.project::setupProject(
   useGit = F,
   functions = "R/getRIA.R",
   # Study area is RIA
+  # studyAreaLarge = {
+  #   reproducible::prepInputs(url = "https://drive.google.com/file/d/1LxacDOobTrRUppamkGgVAUFIxNT4iiHU/view?usp=sharing",
+  #                            destinationPath = "inputs",
+  #                            fun = getRIA,
+  #                            overwrite = TRUE)
+  # },
+  # studyArea = terra::buffer(studyAreaLarge),
+  # rasterToMatch = {
+  #   targetCRS <- terra::crs(studyArea)
+  #   rtm<- terra::rast(studyArea, res = c(250, 250), crs = targetCRS)
+  #   rtm[] <- 1
+  #   rtm <- terra::mask(rtm, studyArea)
+  #   rtm
+  #   },
   studyArea = {
     reproducible::prepInputs(url = "https://drive.google.com/file/d/1LxacDOobTrRUppamkGgVAUFIxNT4iiHU/view?usp=sharing",
                              destinationPath = "inputs",
-                             fun = getRIA,
-                             overwrite = TRUE)
+                             fun = sf::st_read,
+                             overwrite = TRUE) |>
+      terra::vect() |>
+      terra::crop(terra::ext(1150000, 1200000, 1250000, 1300000)) |>
+      terra::aggregate()
   },
   studyAreaLarge = studyArea,
-  speciesToUse = c("Abie_las", "Betu_pap", "Pice_gla", "Pice_mar", "Pinu_con",
-                   "Popu_tre", "Pice_eng"),
-  speciesNameConvention = LandR::equivalentNameColumn(speciesToUse, LandR::sppEquivalencies_CA),
-  sppEquiv = {
-    sppEquiv <- LandR::sppEquivalencies_CA[LandR::sppEquivalencies_CA[[speciesNameConvention]] %in% speciesToUse,]
-    sppEquiv
+  rasterToMatch = {
+    targetCRS <- terra::crs(studyArea)
+    rtm<- terra::rast(studyArea, res = c(250, 250), crs = targetCRS)
+    rtm[] <- 1
+    rtm <- terra::mask(rtm, studyArea)
+    rtm
   },
-  sppColorVect = {
-    sppColorVect <- LandR::sppColors(sppEquiv, speciesNameConvention, palette = "Set1")
-    sppNames <- names(sppColorVect)
-    sppColorVect <- RColorBrewer::brewer.pal(name = "Set1", 8)
-    names(sppColorVect) <- c(sppNames, "Mixed")
-    sppColorVect
+  sppEquiv = {
+    speciesInStudy <- LandR::speciesInStudyArea(studyAreaLarge,
+                                                dPath = "inputs")
+    species <- LandR::equivalentName(speciesInStudy$speciesList, df = LandR::sppEquivalencies_CA, "LandR")
+    sppEquiv <- LandR::sppEquivalencies_CA[LandR %in% species]
+    sppEquiv <- sppEquiv[KNN != "" & LANDIS_traits != ""] #avoid a bug with shore pine
   }
 )
 
