d3609dc45db8067a98ae8049ecd5e7df4e659b5f
DominiqueCaron <dominique.caron@mail.mcgill.ca>
2025-03-26 13:12:08 -0700

    add scfm for small area

--- global.R
+++ global.R
@@ -16,10 +16,52 @@ out <- SpaDES.project::setupProject(
     "PredictiveEcology/Biomass_speciesFactorial@development",
     "PredictiveEcology/Biomass_borealDataPrep@development",
     "PredictiveEcology/Biomass_speciesParameters@development",
+    "PredictiveEcology/Biomass_regeneration@development",
     "DominiqueCaron/Biomass_yieldTables@reorganisation",
     "PredictiveEcology/Biomass_core@main",
-    "DominiqueCaron/LandRCBM_split3pools@restructuration"
+    "DominiqueCaron/LandRCBM_split3pools@restructuration",
+    file.path("PredictiveEcology/scfm@sfContains/modules",
+              c("scfmDataPrep", "scfmIgnition", "scfmEscape", "scfmSpread",
+                "scfmDiagnostics"))
+    
   ),
+  packages = c("googledrive", 'RCurl', 'XML', "stars"),
+  useGit = F,
+  functions = "R/getRIA.R",
+  # Study area is RIA
+  studyArea = {
+    reproducible::prepInputs(
+      url = "https://drive.google.com/file/d/1LxacDOobTrRUppamkGgVAUFIxNT4iiHU/view?usp=sharing",
+      destinationPath = "inputs",
+      fun = getRIA,
+      overwrite = TRUE
+    )|> sf::st_crop(c(xmin = 1150000, xmax = 1200000, ymin = 1250000, ymax = 1300000))
+  }, 
+  studyAreaCalibration = st_buffer(studyArea, 500000),
+  studyAreaLarge = studyArea,
+  rasterToMatch = {
+    targetCRS <- terra::crs(studyArea)
+    rtm <- terra::rast(terra::vect(studyArea), res = c(250, 250))
+    terra::crs(rtm) <- targetCRS
+    rtm[] <- 1
+    rtm <- terra::mask(rtm, terra::vect(studyArea))
+    rtm
+  },
+  rasterToMatchCalibration = {
+    targetCRS <- terra::crs(studyAreaCalibration)
+    rtmc <- terra::rast(terra::vect(studyAreaCalibration), res = c(250, 250))
+    terra::crs(rtmc) <- targetCRS
+    rtmc[] <- 1
+    rtmc <- terra::mask(rtmc, terra::vect(studyAreaCalibration))
+    rtmc
+  },
+  sppEquiv = {
+    speciesInStudy <- LandR::speciesInStudyArea(studyArea,
+                                                dPath = "inputs")
+    species <- LandR::equivalentName(speciesInStudy$speciesList, df = LandR::sppEquivalencies_CA, "LandR")
+    sppEquiv <- LandR::sppEquivalencies_CA[LandR %in% species]
+    sppEquiv <- sppEquiv[KNN != "" & LANDIS_traits != ""] #avoid a bug with shore pine
+  },
   params = list(
     .globals = list(
       dataYear = 2011, #will get kNN 2011 data, and NTEMS 2011 landcover
@@ -47,53 +89,15 @@ out <- SpaDES.project::setupProject(
       moduleNameAndBranch = "PredictiveEcology/Biomass_core@main",
       .plots = "png",
       .useCache = "generateData"
-    )
-  ),
-  packages = c("googledrive", 'RCurl', 'XML', "stars"),
-  useGit = F,
-  functions = "R/getRIA.R",
-  # Study area is RIA
-  studyAreaLarge = {
-    reproducible::prepInputs(
-      url = "https://drive.google.com/file/d/1LxacDOobTrRUppamkGgVAUFIxNT4iiHU/view?usp=sharing",
-      destinationPath = "inputs",
-      fun = getRIA,
-      overwrite = TRUE
-    )
-  }, 
-  studyArea = studyAreaLarge, rasterToMatch = {
-    targetCRS <- terra::crs(studyArea)
-    rtm <- terra::rast(studyArea, res = c(250, 250), crs = targetCRS)
-    rtm[] <- 1
-    rtm <- terra::mask(rtm, studyArea)
-    rtm
-  }, 
-  # studyArea = {
-  #   reproducible::prepInputs(url = "https://drive.google.com/file/d/1LxacDOobTrRUppamkGgVAUFIxNT4iiHU/view?usp=sharing",
-  #                            destinationPath = "inputs",
-  #                            fun = sf::st_read,
-  #                            overwrite = TRUE) |>
-  #     terra::vect() |>
-  #     terra::crop(terra::ext(1150000, 1200000, 1250000, 1300000)) |>
-  #     terra::aggregate()
-  # },
-  # studyAreaLarge = studyArea,
-  rasterToMatch = {
-    targetCRS <- terra::crs(studyArea)
-    rtm<- terra::rast(studyArea, res = c(250, 250), crs = targetCRS)
-    rtm[] <- 1
-    rtm <- terra::mask(rtm, studyArea)
-    rtm
-  },
-  sppEquiv = {
-    speciesInStudy <- LandR::speciesInStudyArea(studyAreaLarge,
-                                                dPath = "inputs")
-    species <- LandR::equivalentName(speciesInStudy$speciesList, df = LandR::sppEquivalencies_CA, "LandR")
-    sppEquiv <- LandR::sppEquivalencies_CA[LandR %in% species]
-    sppEquiv <- sppEquiv[KNN != "" & LANDIS_traits != ""] #avoid a bug with shore pine
-  }
+    ),
+    scfmDataPrep = list(targetN = 4000,
+                        flammabilityThreshold = 0.05,
+                        .useParallelFireRegimePolys = FALSE,
+                        fireEpoch = c(1971, 2020)
+    ),
+    scfmSpread = list(.plotInterval = 10)
+  )
 )
-
 
 out$loadOrder <- unlist(out$modules)
 
