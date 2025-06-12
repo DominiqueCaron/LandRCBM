d3e6aea57e0fd1cc534d1b097c74511cb6577793
DominiqueCaron <dominique.caron@mail.mcgill.ca>
2025-06-12 12:08:52 -0700

    LandRCBM v0.2

--- global_LandRCBM_testData.R
+++ global_LandRCBM_testData.R
@@ -1,6 +1,6 @@
 ###
 ###
-# This script runs LandR and CBM in a small area in RIA.
+# This script runs LandR and CBM in a small area in RIA to create test data.
 ###
 ###
 
@@ -13,26 +13,16 @@ if (!require("SpaDES.project")){
 out <- SpaDES.project::setupProject(
   paths = list(projectPath = getwd(),
                inputPath = "inputs",
-               outputPath = "outputs/LandRCBM_RIAsmall",
+               outputPath = "outputs/LandRCBM_testData",
                cachePath = "cache"),
   options = options(spades.moduleCodeChecks = FALSE,
                     spades.recoveryMode = FALSE),
-  times = list(start = 1985, end = 2085),
+  times = list(start = 2000, end = 2000),
   modules = c(
-    "PredictiveEcology/Biomass_speciesFactorial@development",
     "PredictiveEcology/Biomass_borealDataPrep@development",
-    "PredictiveEcology/Biomass_speciesParameters@development",
     "PredictiveEcology/CBM_defaults@development",
-    "PredictiveEcology/Biomass_regeneration@development",
     "PredictiveEcology/Biomass_yieldTables@main",
-    "PredictiveEcology/Biomass_core@development",
-    "PredictiveEcology/CBM_dataPrep@suz-distRasters",
-    "DominiqueCaron/LandRCBM_split3pools@run-with-CBM",
-    "DominiqueCaron/CBM_core@LandRCBM-simplify-core",
-    file.path("PredictiveEcology/scfm@development/modules",
-              c("scfmDataPrep",
-                "scfmIgnition", "scfmEscape", "scfmSpread",
-                "scfmDiagnostics"))
+    "PredictiveEcology/CBM_dataPrep@development"
   ),
   packages = c("googledrive", 'RCurl', 'XML', "stars", "httr2"),
   useGit = F,
@@ -44,10 +34,9 @@ out <- SpaDES.project::setupProject(
       destinationPath = "inputs",
       fun = getRIA,
       overwrite = TRUE
-    )|> sf::st_crop(c(xmin = 1000000, xmax = 1200000, ymin = 1100000, ymax = 1300000))
+    )|> sf::st_crop(c(xmin = 1018000, xmax = 1020000, ymin = 1200000, ymax = 1202000))
   }, 
-  studyAreaLarge = studyArea,
-  studyAreaCalibration = sf::st_buffer(studyArea, dist = 10^5),
+  studyAreaBiomassParam = sf::st_buffer(studyArea, 10^6),
   rasterToMatch = {
     sa <- terra::vect(studyArea)
     targetCRS <- terra::crs(sa)
@@ -59,17 +48,7 @@ out <- SpaDES.project::setupProject(
   },
   masterRaster = {
     masterRaster = rasterToMatch
-    names(masterRaster) <- "ldSp_TestArea"
     masterRaster
-  },
-  rasterToMatchCalibration = {
-    sac <- terra::vect(studyAreaCalibration)
-    targetCRS <- terra::crs(sac)
-    rtmc <- terra::rast(sac, res = c(250, 250))
-    terra::crs(rtmc) <- targetCRS
-    rtmc[] <- 1
-    rtmc <- terra::mask(rtmc, sac)
-    rtmc
   },
   sppEquiv = {
     speciesInStudy <- LandR::speciesInStudyArea(studyArea,
@@ -78,14 +57,7 @@ out <- SpaDES.project::setupProject(
     sppEquiv <- LandR::sppEquivalencies_CA[LandR %in% species]
     sppEquiv <- sppEquiv[KNN != "" & LANDIS_traits != ""] #avoid a bug with shore pine
   },
-  disturbanceMeta = data.table(
-    eventID = 1,
-    wholeStand = 1,
-    disturbance_type_id = 1,
-    distName = "Wildfire",
-    objectName = "rstCurrentBurn",
-    delay = 1
-  ),
+  disturbanceMeta = data.table(),
   params = list(
     .globals = list(
       dataYear = 2001, #will get kNN 2011 data, and NTEMS 2011 landcover
@@ -94,37 +66,14 @@ out <- SpaDES.project::setupProject(
       sppEquivCol = 'LandR',
       .studyAreaName = "RIA"
     ),
-    CBM_core = list(
-      skipCohortGroupHandling = TRUE,
-      skipPrepareCBMvars = TRUE
-    ),
     Biomass_borealDataPrep = list(
-      overrideAgeInFires = FALSE,
-      overrideBiomassInFires = FALSE,
       .studyAreaName = "RIA",
       subsetDataBiomassModel = 50
-    ),
-    Biomass_speciesFactorial = list(
-      .plots = NULL, #"pdf",
-      runExperiment = TRUE,
-      factorialSize = "medium"
-    ),
-    Biomass_speciesParameters = list(
-      .plots = "png",
-      standAgesForFitting = c(0, 125),
-      .useCache = c(".inputObjects", "init"),
-      speciesFittingApproach = "focal"
     ),
     Biomass_yieldTables = list(
       moduleNameAndBranch = "PredictiveEcology/Biomass_core@development",
       .plots = "png",
       .useCache = "generateData"
-    ),
-    scfmDataPrep = list(targetN = 4000,
-                        flammabilityThreshold = 0.05,
-                        .useParallelFireRegimePolys = FALSE,
-                        fireEpoch = c(1971, 2020),
-                        fireRegimePolysType = "FRU"
     )
   )
 )
